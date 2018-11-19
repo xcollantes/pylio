79e9878e49d18b435401e538e492ab25fff8adc7
xcollantes <xcollantes@zagmail.gonzaga.edu>
2018-11-19 06:02:20 +0000

    change name in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyliomailer",
+    name="pylio",
     version="0.0.1",
     author="Xavier Collantes",
     author_email="xcollantes@zagmail.gonzaga.edu",
